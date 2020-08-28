5494697ad155abc16b1c81e78c68b3a46ceeb7d5
Bradley Guidry <bradleyguidry@gmail.com>
2020-08-27 20:23:53 -0500

    appends input from artict button

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,17 @@
-$(document).ready(function() {
-    $("#headlinerBtn").on("click", function(){
-let LI = createLI();
-LI.addClass("text-3xl");
-$("#headliner").append(LI);
-    })
-    $("#artistBtn").on("click", function(){
-
-    })
-})
-function createLI(){
-var liEl = $("<li>");
-liEl.text($("#user-input").val())
-return liEl;
+$(document).ready(function () {
+  $("#headlinerBtn").on("click", function () {
+    let LI = createLI();
+    LI.addClass("text-3xl");
+    $("#headliner").append(LI);
+  });
+  $("#artistBtn").on("click", function () {
+    let LI = createLI();
+    LI.addClass("inline-block px-3");
+    $("#artist").append(LI);
+  });
+});
+function createLI() {
+  var liEl = $("<li>");
+  liEl.text($("#user-input").val());
+  return liEl;
 }
- 
