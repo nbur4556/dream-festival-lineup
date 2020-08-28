89718ac4336c850a3b1881a5b0ee4bfd954dd07d
Bradley Guidry <bradleyguidry@gmail.com>
2020-08-28 14:19:08 -0500

    adds place for youtube video

--- scripts/script.js
+++ scripts/script.js
@@ -18,5 +18,36 @@ function createLI() {
   });
   return liEl;
   
+  function getVideo() {
+    $.ajax({
+      type: 'GET',
+      url: 'https://www.googleapis.com/youtube/v3/search',
+      data: {
+          key: 'AIzaSyDOkXFMR8ZGNDjvtvYUmbl0Q5_jh2CLCW8',
+          q: "cats",
+          part: 'snippet',
+          maxResults: 1,
+          type: 'video',
+          videoEmbeddable: true,
+      },
+      success: function(data){
+          embedVideo(data)
+      },
+      error: function(response){
+          console.log("Request Failed");
+          
+        }
+      });
+    
+  }
+  function embedVideo(data) {
+    $('iframe').attr('src', 'https://www.youtube.com/embed/' + data.items[0].id.videoId)
+    $('h3').text(data.items[0].snippet.title)
+    $('.description').text(data.items[0].snippet.description)
+    getVideo();
+  }
+  
+
+   
 }
 
