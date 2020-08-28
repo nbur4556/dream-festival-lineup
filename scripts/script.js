50e4f9b574344ead6977e9d17fe13567f7971096
Bradley Guidry <bradleyguidry@gmail.com>
2020-08-27 20:00:27 -0500

    appends headliners

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,16 @@
 $(document).ready(function() {
     $("#headlinerBtn").on("click", function(){
-
+let LI = createLI();
+LI.addClass("text-3xl");
+$("#headliner").append(LI);
     })
     $("#artistBtn").on("click", function(){
-        
+
     })
 })
+function createLI(){
+var liEl = $("<li>");
+liEl.text($("#user-input").val())
+return liEl;
+}
+ 
