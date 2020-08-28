ed6095309c630e2af339e745cfacd1cee20c32b6
nbur4556 <nbur4556@gmail.com>
2020-08-28 17:31:50 -0500

    added ajax call to wikipedia through proxy

--- scripts/script.js
+++ scripts/script.js
@@ -9,55 +9,65 @@ $(document).ready(function () {
     LI.addClass("inline-block px-3");
     $("#artist").append(LI);
   });
-  getVideo();
+  // getVideo();
+  bandModalInfo();
 });
+
 function createLI() {
   var liEl = $("<li>");
   liEl.text($("#user-input").val());
-  liEl.on("click", function(event) {
-    console.log(event)
+  liEl.on("click", function (event) {
+    // console.log(event)
   });
   return liEl;
-  
-  
-    
-  }
-  
-  function getVideo() {
-    $.ajax({
-      type: 'GET',
-      url: 'https://www.googleapis.com/youtube/v3/search',
-      data: {
-          key: 'AIzaSyDOkXFMR8ZGNDjvtvYUmbl0Q5_jh2CLCW8',
-          q: "cats",
-          part: 'snippet',
-          maxResults: 1,
-          type: 'video',
-          videoEmbeddable: true,
-          
-      },
-      
-      success: function(data){
-          embedVideo(data)
-          console.log(data)
-      },
-      error: function(response){
-          console.log("Request Failed");
-          
-        }
-        
-      });
-      
-  }
-  function embedVideo(data) {
-    
-    var videoTag = $("<iframe>");
-    
-    videoTag.attr('src', 'https://www.youtube.com/embed/' + data.items[0].id.videoId);
-    $('h3').text(data.items[0].snippet.title);
-    $('.description').text(data.items[0].snippet.description);
-  
-   $(".videoSection").append(videoTag);
+}
+
+function getVideo() {
+  $.ajax({
+    type: 'GET',
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    data: {
+      key: 'AIzaSyDOkXFMR8ZGNDjvtvYUmbl0Q5_jh2CLCW8',
+      q: "cats",
+      part: 'snippet',
+      maxResults: 1,
+      type: 'video',
+      videoEmbeddable: true,
+
+    },
+
+    success: function (data) {
+      embedVideo(data)
+      // console.log(data)
+    },
+    error: function (response) {
+      console.log("Request Failed");
+
+    }
+
+  });
+
+}
+
+function embedVideo(data) {
+  var videoTag = $("<iframe>");
+
+  videoTag.attr('src', 'https://www.youtube.com/embed/' + data.items[0].id.videoId);
+  $('h3').text(data.items[0].snippet.title);
+  $('.description').text(data.items[0].snippet.description);
+
+  $(".videoSection").append(videoTag);
+}
+
+function bandModalInfo() {
+  const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+
+  $.ajax({
+    url: proxyUrl + "https://www.mediawiki.org/w/api.php?action=query&format=json&titles=Main%20page",
+    method: "GET"
+  }).then(function (data) {
+    console.log(data);
+  });
 }
 
 
