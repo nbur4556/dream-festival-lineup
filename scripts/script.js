9b60cbe8bb6401e0372fc20f92758d0484fd4ede
nbur4556 <nbur4556@gmail.com>
2020-08-29 13:27:28 -0500

    artist name and image is displayed

--- scripts/script.js
+++ scripts/script.js
@@ -65,49 +65,66 @@ function bandModalInfo() {
   let artistImage = $("<img>");
 
   $.ajax({
-    url: "https://rest.bandsintown.com/artists/" +artistInput+ "/?app_id=9ebc2dc78f69f44da1e78195877b2314",
-    method: "GET"
-  }).then(function (data) {
-    console.log(data);
-    // $("#modal-box").text(data.name);//where the band name will appear
-    // artistImage.attr('src', data.image_url);//where the band image will appear
-  });
-  $.ajax({
-    url: "https://rest.bandsintown.com/artists/" +artistInput+ "/events/?app_id=9ebc2dc78f69f44da1e78195877b2314",
+    url: "https://rest.bandsintown.com/artists/" + artistInput + "/?app_id=9ebc2dc78f69f44da1e78195877b2314",
     method: "GET"
   }).then(function (data) {
     console.log(data);
 
+    // $("#modal-box").text(data.name);//where the band name will appear
+    // artistImage.attr('src', data.image_url);//where the band image will appear
+
+    appendArtistToModal(data);
+  });
+  $.ajax({
+    url: "https://rest.bandsintown.com/artists/" + artistInput + "/events/?app_id=9ebc2dc78f69f44da1e78195877b2314",
+    method: "GET"
+  }).then(function (data) {
+    // console.log(data);
+
     dateArr = [data[0].datetime, data[1].datetime, data[2].datetime, data[3].datetime, data[4].datetime];
     citiesArr = [data[0].venue.city, data[1].venue.city, data[2].venue.city, data[3].venue.city, data[4].venue.city];
     statesArr = [data[0].venue.region, data[1].venue.region, data[2].venue.region, data[3].venue.region, data[4].venue.region]
-    console.log(dateArr);
-    console.log(citiesArr);
-    console.log(statesArr);
+    // console.log(dateArr);
+    // console.log(citiesArr);
+    // console.log(statesArr);
+
+
+
     //need a for loop for five shows to display on modal
-    for (var i = 0; i < dateArr.length; i++){
+    for (var i = 0; i < dateArr.length; i++) {
 
     }
 
     // $("").text(data[i].datetime)//date of upcoming show
     // $("").text(data[i].venue.city)//city next show is held
     // $("").text(data[i].venue.region)//state next show is held
-
   });
 
 }
 
-function toggleBox(){
+function appendArtistToModal(data) {
+  const modal = $("#modal-box");
+  const nameItem = $('<h3>');
+  const imageItem = $('<img>');
+
+  nameItem.text(data.name);
+  imageItem.attr('src', data.image_url);
+
+  modal.append(nameItem);
+  modal.append(imageItem);
+}
+
+function toggleBox() {
 
   let modal = $("#modal");
 
-    if (modal.hasClass("visible") === true){
-      modal.removeClass("visible").addClass("invisible");
-    } else if (modal.hasClass("invisible") === true){
-      modal.removeClass("invisible").addClass("visible");
-    }
-    }
-    
+  if (modal.hasClass("visible") === true) {
+    modal.removeClass("visible").addClass("invisible");
+  } else if (modal.hasClass("invisible") === true) {
+    modal.removeClass("invisible").addClass("visible");
+  }
+}
+
 
 
 
