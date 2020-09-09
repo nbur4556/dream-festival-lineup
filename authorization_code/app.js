fd8259dfe1a01c6e33d88dde8ab1e6bc3d323e10
nbur4556 <nbur4556@gmail.com>
2020-09-09 10:00:15 -0500

    set up initial log in page using the spotify api

--- authorization_code/app.js
+++ authorization_code/app.js
@@ -13,16 +13,18 @@ var cors = require('cors');
 var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
 
-var client_id = 'CLIENT_ID'; // Your client id
-var client_secret = 'CLIENT_SECRET'; // Your secret
-var redirect_uri = 'REDIRECT_URI'; // Your redirect uri
+const config = require('../scripts/config.js');
+
+var client_id = config.spotifyCredentials.clientId; // Your client id
+var client_secret = config.spotifyCredentials.clientSecret; // Your secret
+var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
 
 /**
  * Generates a random string containing numbers and letters
  * @param  {number} length The length of the string
  * @return {string} The generated string
  */
-var generateRandomString = function(length) {
+var generateRandomString = function (length) {
   var text = '';
   var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
@@ -37,10 +39,10 @@ var stateKey = 'spotify_auth_state';
 var app = express();
 
 app.use(express.static(__dirname + '/public'))
-   .use(cors())
-   .use(cookieParser());
+  .use(cors())
+  .use(cookieParser());
 
-app.get('/login', function(req, res) {
+app.get('/login', function (req, res) {
 
   var state = generateRandomString(16);
   res.cookie(stateKey, state);
@@ -57,7 +59,7 @@ app.get('/login', function(req, res) {
     }));
 });
 
-app.get('/callback', function(req, res) {
+app.get('/callback', function (req, res) {
 
   // your application requests refresh and access tokens
   // after checking the state parameter
@@ -86,11 +88,11 @@ app.get('/callback', function(req, res) {
       json: true
     };
 
-    request.post(authOptions, function(error, response, body) {
+    request.post(authOptions, function (error, response, body) {
       if (!error && response.statusCode === 200) {
 
         var access_token = body.access_token,
-            refresh_token = body.refresh_token;
+          refresh_token = body.refresh_token;
 
         var options = {
           url: 'https://api.spotify.com/v1/me',
@@ -99,7 +101,7 @@ app.get('/callback', function(req, res) {
         };
 
         // use the access token to access the Spotify Web API
-        request.get(options, function(error, response, body) {
+        request.get(options, function (error, response, body) {
           console.log(body);
         });
 
@@ -119,7 +121,7 @@ app.get('/callback', function(req, res) {
   }
 });
 
-app.get('/refresh_token', function(req, res) {
+app.get('/refresh_token', function (req, res) {
 
   // requesting access token from refresh token
   var refresh_token = req.query.refresh_token;
@@ -133,7 +135,7 @@ app.get('/refresh_token', function(req, res) {
     json: true
   };
 
-  request.post(authOptions, function(error, response, body) {
+  request.post(authOptions, function (error, response, body) {
     if (!error && response.statusCode === 200) {
       var access_token = body.access_token;
       res.send({
--- scripts/config.js
+++ scripts/config.js
@@ -2,4 +2,10 @@ config = {
     bandsintownKey: "9ebc2dc78f69f44da1e78195877b2314",
     youtubeKey: "AIzaSyA7ilPgtslsSUjFGtpcmmJBV8lRCzWjF-s",
     // youtubeKey: 'AIzaSyAq5Pvjkyqvha8J35OkLu5ec5Cdj2zKYjs'
+    spotifyCredentials: {
+        clientId: 'bf41f4ea548649a698141319176993d2',
+        clientSecret: 'f88e1344ea014fea84fd27ab826e2859'
+    }
 }
+
+module.exports = config;
